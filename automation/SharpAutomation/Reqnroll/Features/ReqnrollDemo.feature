3f44d8a0d63ca80e59cc22e5716df80adb456d86
Pavlyuk Dmitro <paulyukdmitro@gmail.com>
2025-05-21 18:43:14 +0300

    Add a reqnroll project

---
+++ automation/SharpAutomation/Reqnroll/Features/ReqnrollDemo.feature
@@ -0,0 +1,6 @@
+﻿Feature: Search on DOU
+
+Scenario: DOU search automation
+    Given I open the DOU homepage
+    When I search for "Automation"
+    Then I should see at least 1 search result
---
+++ automation/SharpAutomation/Reqnroll/Steps/ReqnrollDemoSteps.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using Reqnroll;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpAutomation.Reqnroll.Steps
+{
+    public class ReqnrollDemoSteps
+    {
+        [Binding]
+        public class SearchSteps
+        {
+            private IWebDriver _driver;
+
+            [Given("I open the DOU homepage")]
+            public void OpenDou()
+            {
+                _driver = new ChromeDriver();
+                _driver.Navigate().GoToUrl("https://dou.ua/");
+            }
+
+            [When(@"I search for ""(.*)""")]
+            public void Search(string query)
+            {
+                var input = _driver.FindElement(By.Id("txtGlobalSearch"));
+                input.SendKeys(query + Keys.Enter);
+            }
+
+            [Then("I should see at least 1 search result")]
+            public void VerifyResults()
+            {
+                var results = _driver.FindElements(By.XPath("//div[@class='gsc-webResult gsc-result']"));
+                Assert.That(results.Count, Is.GreaterThan(0));
+                _driver.Quit();
+            }
+        }
+    }
+}
